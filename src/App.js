126589625e75c984d9b3c3af22de084dee60cb0b
Siddhesh <siddhesh.sutar@satelliteHealth.com>
2023-10-19 19:37:18 +0530

    basic slice for habits

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./styles.css";
 import { Provider } from 'react-redux'
 import { store } from "./store";
 import Notifications from "./Notifications/Notifications";
-import CalendarComponent from "./Calendar/Calendar";
 
 export default function App() {
 
@@ -13,7 +12,7 @@ export default function App() {
       </h1>
       <Provider store={store}>
         <div className='content'>
-          <CalendarComponent />
+          Hi
         </div>
         <Notifications />
       </Provider>
---
+++ src/reduxSlices/habitSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  text: '',
+  habits: []
+}
+
+
+export const habitSlice = createSlice({
+  name: 'habit',
+  initialState,
+  reducers: {
+    handleTextChange: (state, action) => {
+      state.text = action.payload
+    },
+    addHabit: (state, action) => {
+      state.habits.push(state.text)
+      state.text = ''
+    },
+    removeHabit: (state, action) => {
+      state.habits = state.habits.filter((item, index) => item.id !== action.payload.id)
+    },
+  }
+})
+
+export const habitReducer = habitSlice.reducer
+
+export const { handleTextChange, addHabit, removeHabit } = habitSlice.actions
+
+export const habitSelector = state => state.habitReducer
